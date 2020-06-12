67c292a8659a025a877e4441ac454cc45fc5348c
Taci Shlosberg <tshlosberg@gmail.com>
2020-06-12 03:17:59 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,30 @@
 def addition(num1, num2)
-
+  add = num1 + num2
+  return add
 end
 
 def subtraction(num1, num2)
-
+  subtract = num2 - num1
+  return subtract
 end
 
 def division(num1, num2)
-
+  divide = num2 / num1
+  return divide
 end
 
 def multiplication(num1, num2)
-
+  multiply = num1 * num2
+  return multiply
 end
 
 def modulo(num1, num2)
-
+  remainder = num2 % num1
+  return remainder
 end
 
 def square_root(num)
-
+  squared = num**
+  return squared
 end
+
